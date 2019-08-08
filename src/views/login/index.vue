5ab16d4378ca56dd94fc01eb9c32ea9cafb8bd50
supeng <18842937759@-email>
2019-08-08 22:28:56 +0800

    init

---
+++ src/views/login/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+
+}
+</script>
+
+<style lang="less">
+// .login-btn {
+//   padding: 20px;
+//   .btn {
+//     width: 100%;
+//   }
+// }
+</style>
